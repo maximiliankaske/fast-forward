cad2b0929fb81d5b28fd9a4c4d7f9e009f17674b
Maximilian Kaske <maximilian@kaske.org>
2021-06-27 11:52:23 +0200

    chore: update google font family

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Oxygen&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Oswald&display=optional"
             rel="stylesheet"
           />
         </Head>
