b93aaeca2261d3c2ae5a2df591e148419498b84d
Maximilian Kaske <maximilian@kaske.org>
2021-06-27 21:05:49 +0200

    fix: badge flexbox

--- components/blog/Thumbnail.tsx
+++ components/blog/Thumbnail.tsx
@@ -5,18 +5,18 @@ import { Post } from "../../types";
 import Badge from "../ui/Badge";
 import { ChevronRightIcon } from "@heroicons/react/solid";
 import Image from "next/image";
+import Heading from "../ui/Heading";
 
-const tags = ["blue", "gray", "red", "pink"] as const;
+const tags = ["blue", "gray", "red", "pink", "purple"] as const;
 
 interface Props {
   post: Post;
 }
 
 const Thumbnail: FC<Props> = ({ post }) => {
-  console.log(post);
   return (
     <article className="flex flex-col md:flex-row rounded-lg hover:shadow-lg border border-transparent hover:border-gray-50 p-4">
-      <div className="flex-1 mr-6 mb-3 w-32 md:w-48">
+      <div className="w-full md:w-48 mr-6 mb-3">
         <h1 className="pb-2">{format(new Date(post.date), "dd.MM.yyyy")}</h1>
         <div className="flex flex-1 items-center space-x-2">
           <div>
@@ -42,16 +42,16 @@ const Thumbnail: FC<Props> = ({ post }) => {
             </a>
           </div>
         </div>
-        <div className="flex space-x-2">
+        <div className="w-full flex flex-wrap">
           {tags.map((i) => (
-            <p key={i}>
-              <Badge color={i}>{i}</Badge>
-            </p>
+            <Badge key={i} color={i} className="mr-2 mb-1">
+              {i}
+            </Badge>
           ))}
         </div>
       </div>
-      <div>
-        <h1 className="text-4xl mb-3 font-semibold">{post.slug}</h1>
+      <div className="flex-1">
+        <Heading as="h2">{post.slug}</Heading>
         <p className="pb-3">{post.excerpt}</p>
         <div className="text-right">
           <Link href={`/posts/${post.slug}`}>
