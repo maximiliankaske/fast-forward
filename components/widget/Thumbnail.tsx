97c5b475574314bcce5bbf84cf806f663bb2954b
Maximilian Kaske <maximilian@kaske.org>
2021-09-02 22:50:31 +0200

    chore: extract componentd + add Loading state

---
+++ components/widget/Thumbnail.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useState } from "react";
+import Button from "../ui/Button";
+import firebase from "firebase/app";
+import html2canvas from "html2canvas";
+import Image from "next/image";
+import { handleUploadState, uploadDataURL } from "../../lib/storage";
+import LoadingIcon from "../icon/Loading";
+import { XIcon } from "@heroicons/react/solid";
+
+const Thumbnail: FC = () => {
+  const [thumbnail, setThumbnail] = useState<string>();
+  const [uploadState, setUploadState] =
+    useState<firebase.storage.UploadTaskSnapshot["state"]>();
+
+  const onScreenShot = () => {
+    html2canvas(document.body).then((canvas) => {
+      const ref = uploadDataURL(`${canvas.toDataURL()}`);
+      handleUploadState(ref, {
+        onSnapshot: (snapshot) => setUploadState(snapshot.state),
+        onComplete: (downloadURL) => {
+          setThumbnail(downloadURL);
+          setUploadState(firebase.storage.TaskState.SUCCESS);
+        },
+      });
+    });
+  };
+
+  const renderState = () => {
+    switch (uploadState) {
+      case firebase.storage.TaskState.RUNNING:
+        return <LoadingIcon className="animate-spin h-5 w-5 text-gray-500" />;
+      case firebase.storage.TaskState.SUCCESS:
+        if (!thumbnail) return null;
+        return (
+          <>
+            <Image layout="fill" src={thumbnail} alt="" objectFit="cover" />
+            <button onClick={() => setUploadState(undefined)}>
+              <XIcon className="absolute -right-2 -top-2 h-4 w-4 text-white bg-red-500 rounded-full" />
+            </button>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="flex items-center space-x-6">
+      <div className="relative h-16 w-16 bg-gray-50 rounded-lg shadow flex items-center justify-center">
+        {renderState()}
+      </div>
+      <Button onClick={onScreenShot}>Take ScreenShot</Button>
+    </div>
+  );
+};
+
+export default Thumbnail;
--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,94 +1,14 @@
-import React, { FC, FormEvent, useState } from "react";
+import React, { FC } from "react";
 import DefaultLayout from "../components/layout/DefaultLayout";
-import Button from "../components/ui/Button";
-import firebase from "firebase/app";
-import { createFeedback } from "../lib/db";
-import Input from "../components/ui/Input";
-import Radios from "../components/ui/Radios";
-import { FeedbackType } from "../types";
-import html2canvas from "html2canvas";
-import Image from "next/image";
-import { handleUploadState, uploadDataURL } from "../lib/storage";
-
-const PROJECT_ID = "bSyoWqKaC9kFEFzpYFpB";
+import Form from "../components/widget/Form";
+import Thumbnail from "../components/widget/Thumbnail";
 
 const Playground: FC = () => {
-  const [thumbnail, setThumbnail] = useState<string>();
-  const [uploadState, setUploadState] =
-    useState<firebase.storage.UploadTaskSnapshot["state"]>();
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      text: { value: string };
-      type: { value: FeedbackType };
-    };
-    try {
-      createFeedback({
-        text: target.text.value,
-        type: target.type.value,
-        projectId: PROJECT_ID,
-        createdAt: firebase.firestore.Timestamp.now(),
-        userAgent: window.navigator.userAgent,
-        location: window.document.location.href,
-      });
-      event.currentTarget.reset();
-    } catch {
-      throw new Error("create Project failed");
-    }
-  };
-
-  const onScreenShot = () => {
-    html2canvas(document.body).then((canvas) => {
-      const ref = uploadDataURL(`${canvas.toDataURL()}`);
-      handleUploadState(ref, {
-        onSnapshot: (snapshot) => setUploadState(snapshot.state),
-        onComplete: (downloadURL) => {
-          setUploadState(firebase.storage.TaskState.SUCCESS);
-          setThumbnail(downloadURL);
-        },
-      });
-    });
-  };
-
-  const renderState = () => {
-    switch (uploadState) {
-      case firebase.storage.TaskState.RUNNING:
-        return <p>Running</p>;
-      case firebase.storage.TaskState.SUCCESS:
-        return <p>Success</p>;
-      default:
-        return <p>None</p>;
-    }
-  };
-
   return (
     <DefaultLayout>
-      <div className="space-y-6">
-        <form onSubmit={onSubmit} className="space-y-4">
-          <Radios
-            label="Type"
-            name="type"
-            options={{
-              issue: {
-                label: "Issue",
-                defaultChecked: true,
-              },
-              other: { label: "Other" },
-            }}
-          />
-          <Input label="Comment" name="text" />
-          <Button reverse type="submit" data-html2canvas-ignore>
-            Submit
-          </Button>
-        </form>
-        <Button onClick={onScreenShot}>ScreenShot</Button>
-        <div className="relative h-32 w-32 bg-gray-100">
-          {thumbnail && (
-            <Image layout="fill" src={thumbnail} alt="" objectFit="contain" />
-          )}
-        </div>
-        {renderState()}
+      <div className="space-y-6 max-w-xl p-6 border rounded shadow">
+        <Form />
+        <Thumbnail />
       </div>
     </DefaultLayout>
   );
