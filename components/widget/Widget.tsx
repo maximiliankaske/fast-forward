79b3514f41b3eaf31771553c8bf39c29ebe25b24
Maximilian Kaske <maximilian@kaske.org>
2021-09-03 19:22:13 +0200

    wip: Widget Modal

---
+++ components/widget/Widget.tsx
@@ -0,0 +1,88 @@
+import React, { Fragment, useRef, useState, useCallback } from "react";
+import { Dialog, Transition, Popover } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+import Button from "../ui/Button";
+
+// TODO: Check how Feedback.fish handles the click on "Give us Feedback" in their React API
+// - Do they have it static or do they include it in the Widget logic?
+// TODO: Replace Dialog by Popover
+
+const Widget = () => {
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
+
+  const onClose = useCallback(() => setOpen(false), []);
+  const onOpen = useCallback(() => setOpen(true), []);
+
+  return (
+    <>
+      <Button onClick={onOpen}>Give us Feedback</Button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          auto-reopen="true"
+          className="fixed z-40 inset-0 overflow-y-auto"
+          onClose={onClose}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6">
+                <div className="block absolute top-0 right-0 pt-4 pr-4">
+                  <button
+                    type="button"
+                    className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-500"
+                    onClick={onClose}
+                    ref={cancelButtonRef}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 sm:mt-0 sm:mx-4">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-center text-lg md:text-xl lg:text-2xl leading-6 font-semibold text-secondary-900"
+                    >
+                      Test
+                    </Dialog.Title>
+                    Hier is the text
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+};
+
+export default Widget;
