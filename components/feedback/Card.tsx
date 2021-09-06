09bfd8c8b7b098ed35094110726e66ca8186c060
Maximilian Kaske <maximilian@kaske.org>
2021-09-06 11:27:19 +0300

    feat: add Modal Component

--- components/feedback/Card.tsx
+++ components/feedback/Card.tsx
@@ -1,10 +1,12 @@
+import { ExternalLinkIcon } from "@heroicons/react/outline";
 import { PhotographIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 import React, { useState } from "react";
 import parser from "ua-parser-js";
 import { Feedback, WithId } from "../../types";
+import Modal from "../common/Modal";
 import Badge from "../ui/Badge";
 import Button from "../ui/Button";
-import Thumbnail from "./Thumbnail";
 
 // TODO: hard coded minutes
 
@@ -39,12 +41,34 @@ const Card = ({ feedback, handleArchive }: Props) => {
             </p>
           </div>
           {feedback?.screenshotURL ? (
-            <div className="col-span-1">
-              <h4 className="uppercase font-semibold">Screenshot</h4>
-              <button onClick={() => setOpen(true)}>
-                <PhotographIcon className="text-gray-500 h-6 w-6" />
-              </button>
-            </div>
+            <>
+              <div className="col-span-1">
+                <h4 className="uppercase font-semibold">Screenshot</h4>
+                <div className="flex space-x-2 items-center">
+                  <button onClick={() => setOpen(true)}>
+                    <PhotographIcon className="text-gray-500 hover:text-gray-600 h-6 w-6" />
+                  </button>
+                  <a
+                    href={feedback.screenshotURL}
+                    download
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <ExternalLinkIcon className="text-gray-500 hover:text-gray-600 h-5 w-5" />
+                  </a>
+                </div>
+              </div>
+              <Modal open={open} onClose={() => setOpen(false)}>
+                <div className="h-64 w-full relative">
+                  <Image
+                    layout="fill"
+                    src={feedback.screenshotURL}
+                    alt=""
+                    objectFit="contain"
+                  />
+                </div>
+              </Modal>
+            </>
           ) : null}
         </div>
         <div className="flex items-center justify-end space-x-3">
@@ -54,9 +78,6 @@ const Card = ({ feedback, handleArchive }: Props) => {
           <Button reverse>Reply with Mail</Button>
         </div>
       </div>
-      <Thumbnail open={open} onClose={() => setOpen(false)}>
-        <div>HEELOO</div>
-      </Thumbnail>
     </>
   );
 };
