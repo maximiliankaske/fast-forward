1e528b0429895db34d17281b96e040b578ed89c4
Maximilian Kaske <maximilian@kaske.org>
2021-09-13 17:49:58 +0300

    feat: add disabled utility classes

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,18 +9,22 @@ const Button: FC<Props> = ({
   children,
   reverse = false,
   className,
+  disabled,
   ...props
 }) => {
   const rootClassName = cn(
     "shadow-sm rounded-md border px-2 py-1",
-    { "bg-white text-gray-900 hover:bg-gray-50": !reverse },
+    { "bg-white text-gray-900": !reverse },
+    { "hover:bg-gray-50": !reverse && !disabled },
     {
-      "bg-gray-800 text-white hover:bg-gray-900": reverse,
+      "bg-gray-800 text-white": reverse,
     },
+    { "hover:bg-gray-900": reverse && !disabled },
+    { "bg-opacity-50 cursor-default": disabled },
     className
   );
   return (
-    <button className={rootClassName} {...props}>
+    <button className={rootClassName} disabled={disabled} {...props}>
       {children}
     </button>
   );
