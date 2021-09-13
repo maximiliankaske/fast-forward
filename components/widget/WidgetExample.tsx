29b4ed4c30266cd94c3ba4b29d0ca57d9425be26
Maximilian Kaske <maximilian@kaske.org>
2021-09-13 20:53:48 +0300

    feat: add reverse widget example button

--- components/widget/WidgetExample.tsx
+++ components/widget/WidgetExample.tsx
@@ -1,14 +1,24 @@
-import React, { FC } from "react";
+import React from "react";
 import Widget from "../../components/widget/Widget";
+import cn from "classnames";
 import { useAuth } from "../../lib/auth";
 
 const PROJECT_ID = "bSyoWqKaC9kFEFzpYFpB";
 
-const WidgetExample: FC = () => {
+interface Props {
+  reverse?: boolean;
+}
+
+const WidgetExample = ({ reverse }: Props) => {
   const { user } = useAuth();
   return (
     <Widget projectId={PROJECT_ID} userId={user?.email || undefined}>
-      <button className="text-indigo-500 hover:text-indigo-600 px-2 py-1">
+      <button
+        className={cn("px-2 py-1", {
+          "text-indigo-500 hover:text-indigo-600": !reverse,
+          "text-white hover:text-gray-100": reverse,
+        })}
+      >
         Give us Feedback
       </button>
     </Widget>
