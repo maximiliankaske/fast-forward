1da56eb558326047bc6eb1e9700168cd57745f3f
Maximilian Kaske <maximilian@kaske.org>
2021-09-14 08:54:51 +0300

    feat(style): newest design

--- pages/app/[projectId]/settings.tsx
+++ pages/app/[projectId]/settings.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState, FormEvent } from "react";
 import useSWR, { mutate } from "swr";
 import DefaultLayout from "../../../components/layout/DefaultLayout";
 import Button from "../../../components/ui/Button";
@@ -12,6 +12,7 @@ import faker from "faker";
 import Input from "../../../components/ui/Input";
 
 const Settings = () => {
+  const [name, setName] = useState("");
   const router = useRouter();
   const { user } = useAuth();
   const { projectId } = router.query;
@@ -21,56 +22,78 @@ const Settings = () => {
     fetcher
   );
 
+  useEffect(() => {
+    if (data?.project) {
+      setName(data.project.name);
+    }
+  }, [data]);
+
   const handleUpdate = useCallback(
-    async (id: string, data: Partial<Project>) => {
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
       try {
-        await updateProject(id, data);
+        // FIXME: make sure that the page is accessed only if data exist
+        await updateProject(data!.project.id, { name });
         mutate([`/api/project/${projectId}`, user!.token]);
       } catch {
         throw new Error("update Project failed");
       }
     },
-    [projectId, user]
+    [projectId, user, data, name]
   );
 
-  const handleDelete = useCallback(
-    async (id: string) => {
-      try {
-        await deleteProject(id);
-        router.replace("/app");
-      } catch {
-        throw new Error("delete Project failed");
-      }
-    },
-    [router]
-  );
+  const handleDelete = useCallback(async () => {
+    try {
+      // FIXME: make sure that the page is accessed only if data exist
+      await deleteProject(data!.project.id);
+      router.replace("/app");
+    } catch {
+      throw new Error("delete Project failed");
+    }
+  }, [router, data]);
 
   return (
     <DefaultLayout>
-      <Heading>Settings</Heading>
-      <div className="border rounded-md">
-        <div className="py-3 px-5">
-          <Input name="name" label="Name" />
-        </div>
-        <div className="py-3 px-5 bg-gray-50 border-t text-right">
-          <Button>Save</Button>
+      <Heading as="h2">Settings</Heading>
+      <div className="space-y-8 mt-8">
+        <form className="border rounded-md" onSubmit={handleUpdate}>
+          <div className="p-5 space-y-1">
+            <h2 className="text-lg leading-6 font-medium">Project settings</h2>
+            <p className="text-sm text-gray-500">
+              Update your billing information. Please note that updating your
+              location could affect your tax rates.
+            </p>
+            <Input
+              name="name"
+              label="Name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div className="py-3 px-5 bg-gray-50 border-t text-right">
+            <Button
+              type="submit"
+              disabled={data?.project.name === name}
+              reverse
+            >
+              Save
+            </Button>
+          </div>
+        </form>
+        <div className="border rounded-md border-red-500">
+          <div className="py-3 px-5 flex justify-between items-center">
+            <div>
+              <h2 className="text-lg leading-6 font-medium">Delete Project</h2>
+              <p className="text-sm text-gray-500">
+                Please note that this is not reversable. Be certain.
+              </p>
+            </div>
+            <Button onClick={handleDelete} className="bg-red-500" reverse>
+              Delete
+            </Button>
+          </div>
         </div>
       </div>
-      {data && (
-        <div>
-          <p>{data.project.name}</p>
-          <Button
-            onClick={() =>
-              handleUpdate(data.project.id, { name: faker.name.firstName() })
-            }
-          >
-            Random Name
-          </Button>
-          <Button onClick={() => handleDelete(data.project.id)}>
-            Delete Project
-          </Button>
-        </div>
-      )}
     </DefaultLayout>
   );
 };
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -4,11 +4,11 @@ import Thumbnail from "../../components/app/Thumbnail";
 import DefaultLayout from "../../components/layout/DefaultLayout";
 import Button from "../../components/ui/Button";
 import Heading from "../../components/ui/Heading";
-import Link from "../../components/ui/Link";
 import { useAuth } from "../../lib/auth";
-import { createProject, deleteProject, updateProject } from "../../lib/db";
+import { createProject } from "../../lib/db";
 import { Project, WithId } from "../../types";
 import fetcher from "../../utils/fetcher";
+import faker from "faker";
 
 const App: FC = () => {
   const { user } = useAuth();
@@ -20,7 +20,7 @@ const App: FC = () => {
   const handleCreate = async () => {
     const newSite = {
       authorId: user!.uid,
-      name: `${Math.random() * 100}`,
+      name: faker.name.firstName(),
     };
     try {
       await createProject(newSite);
@@ -30,30 +30,12 @@ const App: FC = () => {
     }
   };
 
-  const handleUpdateProject = async (id: string) => {
-    try {
-      await updateProject(id, { name: `${Math.random() * 100}` });
-      mutate(["/api/projects", user!.token]);
-    } catch {
-      throw new Error("update Project failed");
-    }
-  };
-
-  const handleDeleteProject = async (id: string) => {
-    try {
-      await deleteProject(id);
-      mutate(["/api/projects", user!.token]);
-    } catch {
-      throw new Error("delete Project failed");
-    }
-  };
-
   return (
     <DefaultLayout>
       <Heading>App</Heading>
       {user && (
         <>
-          <Button onClick={handleCreate}>Add random Project</Button>
+          <Button onClick={handleCreate}>Add Project</Button>
           <Heading as="h3" className="mt-6">
             My Projects
           </Heading>
@@ -62,15 +44,6 @@ const App: FC = () => {
               <Thumbnail key={idx} {...project} />
             ))}
           </div>
-          <hr className="my-10" />
-          {data?.projects.map((i, idx) => (
-            <div key={idx} className="flex space-x-3">
-              <p>{i.name}</p>
-              <button onClick={() => handleUpdateProject(i.id)}>update</button>
-              <button onClick={() => handleDeleteProject(i.id)}>delete</button>
-              <Link href={`/app/${i.id}`}>view</Link>
-            </div>
-          ))}
         </>
       )}
     </DefaultLayout>
