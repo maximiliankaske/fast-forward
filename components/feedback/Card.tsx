d5e74320746fb3a64977325529f5b1777113896e
Maximilian Kaske <maximilian@kaske.org>
2021-09-18 18:09:27 +0200

    refactor: remove modal screenshot thumbnail

--- components/feedback/Card.tsx
+++ components/feedback/Card.tsx
@@ -1,11 +1,8 @@
 import { ExternalLinkIcon } from "@heroicons/react/outline";
-import { PhotographIcon } from "@heroicons/react/solid";
 import { formatDistance } from "date-fns";
-import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import parser from "ua-parser-js";
 import { Feedback, WithId } from "../../types";
-import Modal from "../common/Modal";
 import Badge from "../ui/Badge";
 import Button from "../ui/Button";
 
@@ -15,11 +12,10 @@ interface Props {
 }
 
 const Card = ({ feedback, handleArchive }: Props) => {
-  const [open, setOpen] = useState(false);
   // @ts-ignore FIXME: firebase Timestamp.toDate() not a function
   const createdAtSeconds = feedback.createdAt._seconds * 1000;
   const ua = parser(feedback.userAgent);
-  console.log(feedback?.metadata);
+
   return (
     <div className="border rounded-md shadow-box overflow-hidden">
       <div className="p-6 space-y-3">
@@ -55,30 +51,15 @@ const Card = ({ feedback, handleArchive }: Props) => {
             <>
               <div className="col-span-2 md:col-span-1">
                 <h4 className="uppercase font-semibold">Screenshot</h4>
-                <div className="flex space-x-2 items-center">
-                  <button onClick={() => setOpen(true)}>
-                    <PhotographIcon className="text-gray-500 hover:text-gray-600 h-6 w-6" />
-                  </button>
-                  <a
-                    href={feedback.screenshotURL}
-                    download
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <ExternalLinkIcon className="text-gray-500 hover:text-gray-600 h-5 w-5" />
-                  </a>
-                </div>
+                <a
+                  href={feedback.screenshotURL}
+                  download
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <ExternalLinkIcon className="text-gray-500 hover:text-gray-600 h-5 w-5" />
+                </a>
               </div>
-              <Modal open={open} onClose={() => setOpen(false)}>
-                <div className="h-64 w-full relative">
-                  <Image
-                    layout="fill"
-                    src={feedback.screenshotURL}
-                    alt=""
-                    objectFit="contain"
-                  />
-                </div>
-              </Modal>
             </>
           ) : null}
         </div>
