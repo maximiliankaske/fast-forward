409045cacf5ad413e5120d3ef84beb03d8475a6d
Maximilian Kaske <maximilian@kaske.org>
2021-09-18 18:53:09 +0200

    refactor: remove transition + add popper for auto popover placement

--- components/widget/Widget.tsx
+++ components/widget/Widget.tsx
@@ -1,9 +1,10 @@
-import React, { Fragment, FC } from "react";
-import { Transition, Popover } from "@headlessui/react";
+import React, { FC, useState } from "react";
+import { Popover } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import WidgetForm from "./WidgetForm";
 import { LanguageCode } from "./translations";
 import { Feedback } from "../../types";
+import { usePopper } from "react-popper";
 
 export interface WidgetProps
   extends Pick<Feedback, "userId" | "projectId" | "metadata"> {
@@ -11,40 +12,34 @@ export interface WidgetProps
 }
 
 const Widget: FC<WidgetProps> = ({ children, ...props }) => {
+  let [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>(
+    null
+  );
+  let [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  let { styles, attributes } = usePopper(referenceElement, popperElement, {
+    // placement: "top",
+    modifiers: [{ name: "offset", options: { offset: [0, 8] } }],
+  });
   return (
     <Popover>
-      {({ open }) => (
-        <>
-          <Popover.Button as={Fragment}>{children}</Popover.Button>
-          <Transition
-            show={open}
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-          >
-            <Popover.Panel
-              static
-              className="absolute z-10 pt-2"
-              data-html2canvas-ignore
-            >
-              {({ close }) => (
-                <div className="relative bg-gray-50 border border-gray-100 rounded-md shadow-lg p-4 w-64">
-                  <button
-                    onClick={() => close()}
-                    className="absolute right-2 top-2"
-                  >
-                    <XIcon className="h-4 w-4 text-gray-500" />
-                  </button>
-                  <WidgetForm {...props} />
-                </div>
-              )}
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
+      <Popover.Button as="div" ref={setReferenceElement}>
+        {children}
+      </Popover.Button>
+      <Popover.Panel
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
+        data-html2canvas-ignore
+      >
+        {({ close }) => (
+          <div className="relative bg-gray-50 border border-gray-100 rounded-md shadow-lg p-4 w-64">
+            <button onClick={() => close()} className="absolute right-2 top-2">
+              <XIcon className="h-4 w-4 text-gray-500" />
+            </button>
+            <WidgetForm {...props} />
+          </div>
+        )}
+      </Popover.Panel>
     </Popover>
   );
 };
