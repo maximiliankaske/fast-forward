beee7c4983a6a2fc3f256584e5a93aaddf8e0cb8
Maximilian Kaske <maximilian@kaske.org>
2021-09-19 10:36:11 +0200

    feat: add backdrop filter + sticky header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -17,47 +17,49 @@ const Header: FC = () => {
 
   const isDarkMode = mounted && resolvedTheme === "dark";
   return (
-    <header className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-      <div className="h-16 flex justify-between items-center">
-        <NextLink href="/">
-          <a className="inline-flex text-xs items-end font-extrabold tracking-wider text-pink-500 group hover:text-indigo-500">
-            <FastForwardIcon className="h-8 w-8 text-indigo-500 group-hover:text-pink-500" />
-            fast-forward
-          </a>
-        </NextLink>
-        <div className="space-x-4">
-          <Link href="/docs">Docs</Link>
-          <Link href="/blog">Blog</Link>
-          {auth.user ? (
-            <>
-              <Link href="/app">App</Link>
-              <Link href="/logout">Logout</Link>
-            </>
-          ) : (
-            <Link href="/login">Login</Link>
-          )}
-          <Switch
-            checked={isDarkMode}
-            onChange={() => setTheme(isDarkMode ? "light" : "dark")}
-            className={cn(
-              "relative inline-flex items-center h-6 rounded-full w-11",
-              {
-                "bg-indigo-600": isDarkMode,
-                "bg-gray-200": !isDarkMode,
-              }
+    <header className="sticky top-0 z-20 w-full bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg">
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <div className="h-16 flex justify-between items-center">
+          <NextLink href="/">
+            <a className="inline-flex text-xs items-end font-extrabold tracking-wider text-pink-500 group hover:text-indigo-500">
+              <FastForwardIcon className="h-8 w-8 text-indigo-500 group-hover:text-pink-500" />
+              fast-forward
+            </a>
+          </NextLink>
+          <div className="space-x-4">
+            <Link href="/docs">Docs</Link>
+            <Link href="/blog">Blog</Link>
+            {auth.user ? (
+              <>
+                <Link href="/app">App</Link>
+                <Link href="/logout">Logout</Link>
+              </>
+            ) : (
+              <Link href="/login">Login</Link>
             )}
-          >
-            <span className="sr-only">Enable Dark Mode</span>
-            <span
+            <Switch
+              checked={isDarkMode}
+              onChange={() => setTheme(isDarkMode ? "light" : "dark")}
               className={cn(
-                "inline-block w-4 h-4 transform bg-white rounded-full",
+                "relative inline-flex items-center h-6 rounded-full w-11",
                 {
-                  "translate-x-6": isDarkMode,
-                  "translate-x-1": !isDarkMode,
+                  "bg-indigo-600": isDarkMode,
+                  "bg-gray-200": !isDarkMode,
                 }
               )}
-            />
-          </Switch>
+            >
+              <span className="sr-only">Enable Dark Mode</span>
+              <span
+                className={cn(
+                  "inline-block w-4 h-4 transform bg-white rounded-full",
+                  {
+                    "translate-x-6": isDarkMode,
+                    "translate-x-1": !isDarkMode,
+                  }
+                )}
+              />
+            </Switch>
+          </div>
         </div>
       </div>
     </header>
