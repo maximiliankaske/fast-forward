f9c0e5b4ab00b950c40265deeaf48536720afb23
Maximilian Kaske <maximilian@kaske.org>
2021-09-19 10:36:31 +0200

    fix: main tag flex-1

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -4,9 +4,9 @@ import Header from "./Header";
 
 const DefaultLayout: FC = ({ children }) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-4 flex-1 w-full">
         {children}
       </main>
       <Footer />
