b915ee2817d3ba048d0406dcacf8256fb96bd440
Maximilian Kaske <maximilian@kaske.org>
2021-09-20 21:15:28 +0200

    feat: new Footer

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -6,7 +6,7 @@ const DefaultLayout: FC = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-4 flex-1 w-full">
+      <main className="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-12 flex-1 w-full">
         {children}
       </main>
       <Footer />
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,12 +1,41 @@
 import React, { FC } from "react";
+import GitHubIcon from "../icon/GitHub";
+import TwitterIcon from "../icon/Twitter";
+import Heading from "../ui/Heading";
 import Link from "../ui/Link";
+import Header from "./Header";
 
 const Footer: FC = () => {
   return (
-    <footer className="flex justify-center items-center space-x-4 my-8">
-      <Link href="/docs">Privacy</Link>
-      <Link href="/docs">Terms</Link>
-      <Link href="/">Home</Link>
+    <footer className="w-full max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <div className="pt-8 pb-16 grid grid-cols-2 border-t">
+        <div className="space-y-4">
+          <div>
+            <Link href="/docs">Privacy</Link>
+          </div>
+          <div>
+            <Link href="/docs">Terms</Link>
+          </div>
+          <div>
+            <Link href="/docs">Home</Link>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <p className="uppercase text-sm font-semibold">Community</p>
+          <div>
+            <Link href="/docs" className="inline-flex items-center">
+              <TwitterIcon className="h-4 w-4 mr-1" />
+              Twitter
+            </Link>
+          </div>
+          <div>
+            <Link href="/docs" className="inline-flex items-center">
+              <GitHubIcon className="h-4 w-4 mr-1" />
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
