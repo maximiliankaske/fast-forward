02a16a2477e0cb458d3cb883f3109000b1988f26
Maximilian Kaske <maximilian@kaske.org>
2021-09-20 23:30:21 +0200

    wip: remove className

--- utils/toasts.ts
+++ utils/toasts.ts
@@ -1,7 +1,5 @@
 import toast from "react-hot-toast";
 
 export function feedbackErrorToast() {
-  toast.error("Update Feedback failed. Please be authorized.", {
-    className: "bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-100",
-  });
+  toast.error("Update Feedback failed. Please be authorized.");
 }
