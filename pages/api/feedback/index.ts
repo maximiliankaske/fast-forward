5965082c64ed6f5e2bdb0473bc7affdf807e56dd
Maximilian Kaske <maximilian@kaske.org>
2021-09-23 21:26:57 +0200

    wtf: ok so this seems to work?

--- pages/api/feedback/index.ts
+++ pages/api/feedback/index.ts
@@ -8,12 +8,13 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("body", req.body);
     const [error, feedback] = validate(req.body, Feedback);
     console.log(feedback, error);
-    if (feedback) {
-      const { feedback: f } = await createFeedback(feedback);
-      return res.status(200).json({ ...f });
-    } else {
-      return res.status(422).json({ error });
-    }
+    // if (feedback) {
+    //   const { feedback: f } = await createFeedback(feedback);
+    //   return res.status(200).json({ ...f });
+    // } else {
+    //   return res.status(422).json({ error });
+    // }
+    return res.status(200).json({});
   } catch (error) {
     return res.status(500).json({ error });
   }
