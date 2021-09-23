af133424c7ed6cc10f109d309c740ffa69ed4951
Maximilian Kaske <maximilian@kaske.org>
2021-09-23 21:32:28 +0200

    fix: preflight error

--- pages/api/feedback/index.ts
+++ pages/api/feedback/index.ts
@@ -5,16 +5,18 @@ import { Feedback } from "../../../types/superstruct";
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    console.log("body", req.body);
-    const [error, feedback] = validate(req.body, Feedback);
-    console.log(feedback, error);
-    // if (feedback) {
-    //   const { feedback: f } = await createFeedback(feedback);
-    //   return res.status(200).json({ ...f });
-    // } else {
-    //   return res.status(422).json({ error });
-    // }
-    return res.status(200).json({});
+    // REMINDER: cors api call first creates a "preflight" with method: "OPTIONS"
+    // if no switch - case, validation will fail
+    switch (req.method) {
+      case "POST":
+        const [error, feedback] = validate(req.body, Feedback);
+        if (feedback) {
+          const { feedback: f } = await createFeedback(feedback);
+          return res.status(200).json({ ...f });
+        } else {
+          return res.status(422).json({ error });
+        }
+    }
   } catch (error) {
     return res.status(500).json({ error });
   }
