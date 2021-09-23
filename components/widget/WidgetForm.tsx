698b75547d8eb47fb9ce8fcf591d17e0f9f830bd
Maximilian Kaske <maximilian@kaske.org>
2021-09-23 22:15:18 +0200

    chore: promise toast + form state

--- components/widget/WidgetForm.tsx
+++ components/widget/WidgetForm.tsx
@@ -1,4 +1,10 @@
-import React, { FormEvent, useCallback, useRef, useState } from "react";
+import React, {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import Button from "../ui/Button";
 import Radios from "../ui/Radios";
 import { FeedbackType } from "../../types";
@@ -6,57 +12,88 @@ import Thumbnail from "./Thumbnail";
 import { WidgetProps } from "./Widget";
 import TextArea from "../ui/TextArea";
 import { formattedMessages } from "./translations";
+import { sendFeedbackPromiseToast } from "../../utils/toasts";
+import LoadingIcon from "../icon/Loading";
+import { CheckIcon } from "@heroicons/react/solid";
 
 const WidgetForm = ({
   userId,
   projectId,
   lang: defaultLang,
   metadata,
+  domain,
 }: WidgetProps) => {
+  const [form, setForm] = useState<"idle" | "pending" | "error" | "success">(
+    "idle"
+  );
   const formRef = useRef<HTMLFormElement>(null);
   const [screenshotURL, setScreenshotURL] = useState<string>();
   const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    if (text !== "" && form !== "idle") {
+      setForm("idle");
+    }
+  }, [text, form]);
 
   const handleReset = useCallback(() => {
     formRef.current?.reset();
     setText("");
     setScreenshotURL(undefined);
+    setForm("success");
   }, []);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setForm("pending");
     const target = event.target as typeof event.target & {
       text: { value: string };
       type: { value: FeedbackType };
     };
     try {
-      await fetch("/api/feedback", {
-        method: "POST",
-        headers: new Headers({ "Content-Type": "application/json" }),
-        body: JSON.stringify({
-          text: target.text.value,
-          type: target.type.value,
-          projectId,
-          userAgent: window.navigator.userAgent,
-          location: window.document.location.href,
-          metadata,
-          userId,
-          screenshotURL,
-          // Append to Object only if !undefined
-          // ...(screenshotURL && { screenshotURL }),
-          // ...(userId && { userId }),
-          // ...(metadata && { metadata }),
-        }),
-      });
+      // REMINDER: remove sendFeedbackPromiseToast later
+      await sendFeedbackPromiseToast(
+        fetch(`${domain || ""}/api/feedback`, {
+          method: "POST",
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+          body: JSON.stringify({
+            text: target.text.value,
+            type: target.type.value,
+            projectId,
+            userAgent: window.navigator.userAgent,
+            location: window.document.location.href,
+            metadata,
+            userId,
+            screenshotURL,
+          }),
+        })
+      );
       handleReset();
-    } catch {
-      throw new Error("create Project failed");
+    } catch (error) {
+      setForm("error");
     }
   };
 
   const messages = formattedMessages(
     defaultLang || document.documentElement.lang || "en"
   );
+
+  const renderState = () => {
+    switch (form) {
+      case "idle":
+        return messages.submit.label;
+      case "pending":
+        return (
+          <LoadingIcon className="h-4 w-4 my-1 animate-spin text-gray-500 mx-auto" />
+        );
+      case "error":
+        return "error";
+      case "success":
+        return <CheckIcon className="h-4 w-4 my-1 text-green-500 mx-auto" />;
+    }
+  };
 
   return (
     <form ref={formRef} onSubmit={handleSubmit} className="space-y-3">
@@ -92,7 +129,7 @@ const WidgetForm = ({
           disabled={text === ""}
           size="sm"
         >
-          {messages.submit.label}
+          {renderState()}
         </Button>
       </div>
     </form>
--- utils/toasts.ts
+++ utils/toasts.ts
@@ -4,6 +4,18 @@ export function feedbackErrorToast() {
   toast.error("Update Feedback failed. Please be authorized.");
 }
 
+export function sendFeedbackErrorToast() {
+  toast.error("Failed to send feedback.");
+}
+
+export function sendFeedbackPromiseToast(promise: Promise<unknown>) {
+  return toast.promise(promise, {
+    loading: "Loading",
+    success: "Got the data",
+    error: "Error when fetching",
+  });
+}
+
 export function feedbackResetToast() {
   toast.success("Reset Feedbacks.");
 }
