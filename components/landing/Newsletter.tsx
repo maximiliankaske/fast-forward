99f43758d3ef4c7f89b73f734c459b75e74f3516
Maximilian Kaske <maximilian@kaske.org>
2021-09-25 20:33:31 +0200

    feat: subscriptions to newsletter create document in firebase

--- components/landing/Newsletter.tsx
+++ components/landing/Newsletter.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { FormEvent, useCallback, useRef, useState } from "react";
+import { addNewsletterSubscription } from "../../lib/db";
 import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Input from "../ui/Input";
 
 const Newsletter = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [form, setForm] = useState<"idle" | "loading" | "error" | "success">(
+    "idle"
+  );
+  const handleSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setForm("loading");
+      const target = event.target as typeof event.target & {
+        email: { value: string };
+      };
+      try {
+        await addNewsletterSubscription(target.email.value);
+        setForm("success");
+        formRef.current?.reset();
+      } catch (error) {
+        setForm("error");
+      }
+    },
+    []
+  );
   return (
     <div className="p-4 border rounded-md bg-gray-50 dark:bg-gray-900">
       <Heading as="h3">Subscribe to the newsletter</Heading>
@@ -11,22 +33,36 @@ const Newsletter = () => {
         Get emails from me about web development, tech, and early access to new
         articles.
       </p>
-      <div className="flex space-x-2">
+      <form ref={formRef} className="flex space-x-2" onSubmit={handleSubmit}>
         <Input
           label="Email"
           name="email"
+          type="email"
           srOnly
           placeholder="maximilian@kaske.org"
           className="sm:w-64"
+          required
         />
         <div className="mt-1 flex">
-          <Button reverse className="pl-4 pr-4">
-            Submit
+          <Button type="submit" reverse className="pl-4 pr-4">
+            {form === "loading" ? "Loading..." : "Subscribe"}
           </Button>
         </div>
-      </div>
-      <p className="text-red-500 pt-2 text-sm font-medium">
-        Not working at the moment
+      </form>
+      <p className="pt-2 text-sm font-medium">
+        {form === "error" ? (
+          <span className="text-red-800 dark:text-red-400">
+            Something went wrong!
+          </span>
+        ) : form === "success" ? (
+          <span className="text-green-700 dark:text-green-400">
+            {`Hooray! You're now on the list.`}
+          </span>
+        ) : (
+          <span className="text-gray-600 dark:text-gray-400">
+            {`Lets make it great. Now.`}
+          </span>
+        )}
       </p>
     </div>
   );
--- lib/db.ts
+++ lib/db.ts
@@ -95,3 +95,10 @@ export function updateFeedback(
     .doc(id)
     .update(data);
 }
+
+export function addNewsletterSubscription(email: string) {
+  return firebase.firestore().collection("subscriptions").doc(email).set({
+    email,
+    createdAt: firebase.firestore.Timestamp.now(),
+  });
+}
