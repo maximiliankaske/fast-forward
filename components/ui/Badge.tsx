1929a20d1e88581dc45ebc807356dbe7ad357dce
Maximilian Kaske <maximilian@kaske.org>
2021-09-28 22:45:30 +0200

    feat: add dark mode colors

--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -9,12 +9,18 @@ const Badge: FC<Props> = ({ children, className, color = "gray" }) => {
   const rootClassName = cn(
     "inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium",
     {
-      "bg-gray-100 text-gray-800": color === "gray",
-      "bg-blue-100 text-blue-800": color === "blue",
-      "bg-red-100 text-red-800": color === "red",
-      "bg-green-100 text-green-800": color === "green",
-      "bg-purple-100 text-purple-800": color === "purple",
-      "bg-pink-100 text-pink-800": color === "pink",
+      "bg-gray-100 dark:bg-gray-600 text-gray-800 dark:text-gray-200":
+        color === "gray",
+      "bg-blue-100 dark:bg-blue-600 text-blue-800 dark:text-blue-200":
+        color === "blue",
+      "bg-red-100 dark:bg-red-600 text-red-800 dark:text-red-200":
+        color === "red",
+      "bg-green-100 dark:bg-green-600 text-green-800 dark:text-green-200":
+        color === "green",
+      "bg-purple-100 dark:bg-purple-600 text-purple-800 dark:text-purple-200":
+        color === "purple",
+      "bg-pink-100 dark:bg-pink-600 text-pink-800 dark:text-pink-200":
+        color === "pink",
     },
     className
   );
