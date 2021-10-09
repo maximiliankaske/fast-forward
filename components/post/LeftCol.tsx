59bcdabcba03516c4c78cbf866e3148b53c494f5
Maximilian Kaske <maximilian@kaske.org>
2021-10-09 15:37:34 +0200

    fix: tsc deployment error

--- components/post/LeftCol.tsx
+++ components/post/LeftCol.tsx
@@ -16,12 +16,12 @@ const LeftCol: FC<Props> = ({ post }) => {
   return (
     // self-start because in flexbox
     <div className="mb-4 xl:sticky top-20 self-start hidden xl:block">
-      <Avatar
+      {/* <Avatar
         imgSrc={post.authorPicture}
         name={post.authorName}
         some="@twitter"
         className="mt-4"
-      />
+      /> */}
       <Divider className="py-6" />
       <p className="text-xs leading-5 tracking-wide uppercase text-gray-600 dark:text-gray-400 pb-2">
         Defined Tags
