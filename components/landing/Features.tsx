a2c99d3bc64b0a7a51c818d153dab7060742b1a6
Maximilian Kaske <maximilian@kaske.org>
2021-10-12 22:05:22 +0200

    styles: yellow to indigo

--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -18,7 +18,7 @@ const features = [
   },
   {
     name: "Translation",
-    backgroundColor: "bg-yellow-400",
+    backgroundColor: "bg-indigo-500",
     description:
       "It is very easy to change the language depending on the locales you support. Locales supported: <b>en</b>, <b>de</b>, <b>fr</b>.",
     icon: GlobeAltIcon,
