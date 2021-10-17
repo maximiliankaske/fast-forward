7c6e75085163ff01d6f255f0a25a8feeeafa29b6
Maximilian Kaske <maximilian@kaske.org>
2021-10-17 17:32:18 +0200

    style: button spacing

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,7 +15,7 @@ const Login = () => {
       <div className="mx-auto max-w-xl my-16 space-y-6 text-center">
         <Heading as="h2">Login Options</Heading>
         <div className="text-gray-600 dark:text-gray-400 space-y-4">
-          <p>
+          <p className="text-sm">
             Lets be serious, why would you want to create a new account with
             email and password just to let either your password manager handle
             it or to forget the credentials after the logout. Let the providers
@@ -23,26 +23,28 @@ const Login = () => {
           </p>
           <p className="font-medium">Choose yours:</p>
         </div>
-        <div>
-          <Button
-            onClick={() =>
-              auth.signinWithGitHub().then(() => router.replace("/app"))
-            }
-            className="inline-flex items-center"
-            reverse
-          >
-            Log In with GitHub <GitHubIcon className="h-5 w-5 -mr-1 ml-1" />
-          </Button>
-        </div>
-        <div>
-          <Button
-            onClick={() =>
-              auth.signinWithGoogle().then(() => router.replace("/app"))
-            }
-            className="inline-flex items-center"
-          >
-            Log In with Google <GoogleIcon className="h-5 w-5 -mr-1 ml-1" />
-          </Button>
+        <div className="space-y-4">
+          <div>
+            <Button
+              onClick={() =>
+                auth.signinWithGitHub().then(() => router.replace("/app"))
+              }
+              className="inline-flex items-center"
+              reverse
+            >
+              Log In with GitHub <GitHubIcon className="h-5 w-5 -mr-1 ml-1" />
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() =>
+                auth.signinWithGoogle().then(() => router.replace("/app"))
+              }
+              className="inline-flex items-center"
+            >
+              Log In with Google <GoogleIcon className="h-5 w-5 -mr-1 ml-1" />
+            </Button>
+          </div>
         </div>
       </div>
     </DefaultLayout>
