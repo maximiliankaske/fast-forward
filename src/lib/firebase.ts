91020ecc444d65573d83761b3713bb5cebef743b
Maximilian Kaske <maximilian@kaske.org>
2021-10-25 21:27:22 +0200

    test: add firebase emulator

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/functions";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -20,6 +21,13 @@ if (typeof window !== "undefined") {
     // firebase.analytics();
     // firebase.performance();
   }
+}
+
+if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
+  firebase.firestore().useEmulator("localhost", 8080);
+  firebase.functions().useEmulator("localhost", 5001);
+  firebase.auth().useEmulator("http://localhost:9099");
+  firebase.storage().useEmulator("localhost", 9199);
 }
 
 export default firebase;
