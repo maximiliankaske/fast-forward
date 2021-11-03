24753c9f9b16de6d4ef1d88f29912a09d5219fd7
Maximilian Kaske <maximilian@kaske.org>
2021-11-04 00:32:45 +0100

    wip: big improvements in code design

--- src/pages/_sites/[site]/form.tsx
+++ src/pages/_sites/[site]/form.tsx
@@ -14,6 +14,7 @@ import Input from "@/components/question/Input";
 import Link from "@/components/ui/Link";
 import Question from "@/components/question/Question";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/solid";
+import cn from "classnames";
 
 // FIXME: right now is ?session=docIdx but if user starts new form, it will create new doc
 // TODO: think of a reducer `useReducer()`
@@ -24,8 +25,8 @@ const { questions: questions } = pack;
 const FormPage = ({
   organization,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [index, setIndex] = useState<number | undefined>();
-  const [question, setQuestion] = useState<QuestionType | undefined>();
+  const [index, setIndex] = useState<number>(0);
+  // const [question, setQuestion] = useState<QuestionType | undefined>();
   const router = useRouter();
   const { user } = useAuth();
   const { session } = router.query as { session: string };
@@ -41,11 +42,12 @@ const FormPage = ({
     fetcher
   );
 
-  console.log({ question });
+  const answeredQuestionIds = useMemo(
+    () => (data?.session?.answers ? Object.keys(data.session.answers) : []),
+    [data?.session?.answers]
+  );
 
-  const answeredQuestionIds = data?.session?.answers
-    ? Object.keys(data.session.answers)
-    : undefined;
+  const question = index ? questions[index] : questions[0];
 
   const missingQuestionIds = useMemo(
     () =>
@@ -60,16 +62,9 @@ const FormPage = ({
   );
 
   useEffect(() => {
-    console.log({ data: data?.session?.answers, missingQuestionIds });
-    if (missingQuestionIds.length > 0) {
-      setQuestion(questions.find(({ id }) => missingQuestionIds[0] === id));
-    } else if (missingQuestionIds.length === 0) {
-      setQuestion(undefined);
-    }
     setIndex(
       questions.findIndex((question) => missingQuestionIds[0] === question.id)
     );
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.session?.answers]);
 
@@ -80,16 +75,20 @@ const FormPage = ({
       name: { value: string };
     };
     if (question) {
-      await updateSession({
-        organizationId: organization.id,
-        id: session, // data.id is better
-        // Ideally, we use the `answers.${question.id}`: target.name.value
-        answers: {
-          ...data?.session?.answers,
-          [question.id]: target.name.value,
-        },
-      });
-      mutate();
+      if (target.name.value !== data?.session?.answers[question.id]) {
+        await updateSession({
+          organizationId: organization.id,
+          id: session, // data.id is better
+          // Ideally, we use the `answers.${question.id}`: target.name.value
+          answers: {
+            ...data?.session?.answers,
+            [question.id]: target.name.value,
+          },
+        });
+        mutate();
+      } else {
+        setIndex((prev) => prev + 1);
+      }
     }
   };
 
@@ -97,52 +96,74 @@ const FormPage = ({
 
   return (
     <SitesLayout name={organization.name}>
+      <div className="flex items-center justify-between">
+        <div className="flex space-x-2">
+          {questions.map((question, idx) => {
+            const disabled = idx > index && idx > answeredQuestionIds.length;
+            return (
+              <button
+                key={question.id}
+                // Define how do show the user until which question he has answered and not.
+                // [closed][closed][current][answered][open]
+                // With different colors per question state.
+                // Only use very flatten bordered rectangle with (background if open, primary if current, foreground if closed)
+                // Let’s make it hoverable with a tooltip showing the question
+                className={cn("h-2 w-16 rounded-full", {
+                  "bg-gray-800":
+                    index > idx && idx < answeredQuestionIds.length,
+                  "bg-indigo-500 dark:bg-pink-500": index === idx,
+                  "bg-white": index < idx && idx > answeredQuestionIds.length,
+                  "cursor-default": disabled,
+                  // "bg-gray-500": idx > index && idx > answeredQuestionIds.length,
+                })}
+                disabled={disabled}
+                onClick={() => setIndex(idx)}
+              />
+            );
+          })}
+        </div>
+        {index < answeredQuestionIds.length ? (
+          <button
+            onClick={() => setIndex(answeredQuestionIds.length)}
+            className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 inline-flex items-center"
+          >
+            Go to last
+            <ArrowRightIcon className="h-4 w-4 ml-1" />
+          </button>
+        ) : null}
+      </div>
       {question ? (
-        <>
-          <p className="text-lg text-gray-600 dark:text-gray-400">
-            Question{" "}
-            <span className="font-bold">
-              {(answeredQuestionIds?.length || 0) + 1}
-            </span>{" "}
-            of {questions.length}
-          </p>
-          <form onSubmit={onSubmit} className="space-y-12 py-8">
-            <Question
-              title={question.title}
-              description={question.description}
-            />
-            <Input />
-            <div className="flex items-center justify-between">
-              <div>
-                {answeredQuestionIds && answeredQuestionIds.length > 0 && (
-                  <button
-                    type="button"
-                    // FIXME: Not the right way. you can only fo back one question.
-                    onClick={() => {
-                      setQuestion(
-                        questions.find(
-                          ({ id }) => answeredQuestionIds[0] === id
-                        )
-                      );
-                      setIndex((index || 0) - 1);
-                    }}
-                    className="text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-600"
-                  >
-                    <ArrowLeftIcon className="h-8 w-8" />
-                  </button>
-                )}
-              </div>
-              {missingQuestionIds && (
+        <form onSubmit={onSubmit} className="space-y-12 py-8">
+          <Question title={question.title} description={question.description} />
+          <Input />
+          <div className="flex items-center justify-between">
+            <div>
+              {index > 0 && (
                 <button
-                  type="submit"
-                  className="rounded-full p-2 bg-gray-600 dark:bg-text-400 hover:bg-gray-800 dark:hover:bg-gray-600 text-white"
+                  type="button"
+                  onClick={() => setIndex((index || 0) - 1)}
+                  className="rounded-full p-2 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900"
                 >
-                  <ArrowRightIcon className="h-7 w-7" />
+                  <ArrowLeftIcon className="h-7 w-7" />
                 </button>
               )}
             </div>
-          </form>
-        </>
+            {missingQuestionIds && (
+              <button
+                type="submit"
+                className={cn("rounded-full p-2 text-white", {
+                  // needs to be elaborated - not correct numbers
+                  "bg-gray-600 hover:bg-gray-800 dark:hover:bg-gray-700":
+                    index !== questions.length - 1,
+                  "bg-indigo-500 dark:bg-pink-500 hover:bg-indigo-600 dark:hover:bg-pink-600":
+                    index === questions.length - 1,
+                })}
+              >
+                <ArrowRightIcon className="h-7 w-7" />
+              </button>
+            )}
+          </div>
+        </form>
       ) : (
         <>
           <Heading>Thanks to attempt the form!</Heading>
