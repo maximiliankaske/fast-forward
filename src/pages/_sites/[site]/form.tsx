4533d07c2f1b7763a6c2e6e1bbf2500a63eb3688
Maximilian Kaske <maximilian@kaske.org>
2021-11-03 22:57:54 +0100

    wip: save

--- src/pages/_sites/[site]/form.tsx
+++ src/pages/_sites/[site]/form.tsx
@@ -8,10 +8,12 @@ import fetcher from "@/utils/fetcher";
 import { useAuth } from "@/lib/auth";
 import { FormEvent, useEffect, useMemo, useState } from "react";
 import { updateSession } from "@/lib/db";
-import formsConfig, { Question } from "@/config/forms";
+import formsConfig, { Question as QuestionType } from "@/config/forms";
 import Heading from "@/components/ui/Heading";
-import Form from "@/components/question/Form";
+import Input from "@/components/question/Input";
 import Link from "@/components/ui/Link";
+import Question from "@/components/question/Question";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/solid";
 
 // FIXME: right now is ?session=docIdx but if user starts new form, it will create new doc
 // TODO: think of a reducer `useReducer()`
@@ -22,7 +24,8 @@ const { questions: questions } = pack;
 const FormPage = ({
   organization,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [question, setQuestion] = useState<Question | undefined>();
+  const [index, setIndex] = useState<number | undefined>();
+  const [question, setQuestion] = useState<QuestionType | undefined>();
   const router = useRouter();
   const { user } = useAuth();
   const { session } = router.query as { session: string };
@@ -37,6 +40,8 @@ const FormPage = ({
       : null,
     fetcher
   );
+
+  console.log({ question });
 
   const answeredQuestionIds = data?.session?.answers
     ? Object.keys(data.session.answers)
@@ -55,14 +60,20 @@ const FormPage = ({
   );
 
   useEffect(() => {
+    console.log({ data: data?.session?.answers, missingQuestionIds });
     if (missingQuestionIds.length > 0) {
       setQuestion(questions.find(({ id }) => missingQuestionIds[0] === id));
-    } else {
+    } else if (missingQuestionIds.length === 0) {
       setQuestion(undefined);
     }
-  }, [data?.session, data?.session?.answers, missingQuestionIds]);
+    setIndex(
+      questions.findIndex((question) => missingQuestionIds[0] === question.id)
+    );
 
-  const onSubmit = async (event: FormEvent) => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.session?.answers]);
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     // TODO: event.reset form
     event.preventDefault();
     const target = event.target as typeof event.target & {
@@ -82,6 +93,8 @@ const FormPage = ({
     }
   };
 
+  console.log(index);
+
   return (
     <SitesLayout name={organization.name}>
       {question ? (
@@ -93,7 +106,42 @@ const FormPage = ({
             </span>{" "}
             of {questions.length}
           </p>
-          <Form question={question} onSubmit={onSubmit} />
+          <form onSubmit={onSubmit} className="space-y-12 py-8">
+            <Question
+              title={question.title}
+              description={question.description}
+            />
+            <Input />
+            <div className="flex items-center justify-between">
+              <div>
+                {answeredQuestionIds && answeredQuestionIds.length > 0 && (
+                  <button
+                    type="button"
+                    // FIXME: Not the right way. you can only fo back one question.
+                    onClick={() => {
+                      setQuestion(
+                        questions.find(
+                          ({ id }) => answeredQuestionIds[0] === id
+                        )
+                      );
+                      setIndex((index || 0) - 1);
+                    }}
+                    className="text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-600"
+                  >
+                    <ArrowLeftIcon className="h-8 w-8" />
+                  </button>
+                )}
+              </div>
+              {missingQuestionIds && (
+                <button
+                  type="submit"
+                  className="rounded-full p-2 bg-gray-600 dark:bg-text-400 hover:bg-gray-800 dark:hover:bg-gray-600 text-white"
+                >
+                  <ArrowRightIcon className="h-7 w-7" />
+                </button>
+              )}
+            </div>
+          </form>
         </>
       ) : (
         <>
