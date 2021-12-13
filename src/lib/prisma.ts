eda24e89495736afadec9edf1d8a9ae5f695cb07
Maximilian Kaske <maximilian@kaske.org>
2021-12-13 21:01:22 +0100

    refactor: prisma global

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient({
-    // log: ["query", "info", "warn", "error"],
-  });
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      // log: ["query", "info", "warn", "error"],
-    });
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
