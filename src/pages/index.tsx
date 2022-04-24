d588a72af6d39c94d3b6351f900ffab5c9dce08c
Maximilian Kaske <maximilian@kaske.org>
2022-04-24 11:36:10 +0200

    chore: little change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,19 @@ const Home = () => {
     <DefaultLayout>
       <div className="grid md:grid-cols-5 gap-x-0 md:gap-x-6 pt-6 md:pt-12 pb-24 md:pb-40">
         <div className="md:col-span-3">
-          <Heading>The easiest way to collect Feedback.</Heading>
+          <Heading>
+            The easiest way to collect{" "}
+            <span className="animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-transparent font-extrabold">
+              Feedback
+            </span>
+            .
+          </Heading>
         </div>
         <div className="md:col-span-2 hidden md:block" />
         <div className="md:col-span-3">
           <Heading as="h4">
             {`You want to collect feedback fast, reliable and without any big setup? Welcome to `}
-            <span className="animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-transparent font-extrabold">
+            <span className="font-extrabold text-indigo-500 dark:text-pink-500">
               Fast Forward
             </span>
             .
