d0a1af99ab29cfb36e23ff448ad9fc4719845fb0
Maximilian Kaske <maximilian@kaske.org>
2022-05-10 23:17:47 +0200

    fix: build

--- apps/web/src/lib/markdownToHtml.ts
+++ apps/web/src/lib/markdownToHtml.ts
@@ -4,6 +4,7 @@ import prism from "remark-prism";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
+    // @ts-ignore FIXME:
     .use(html, { sanitize: false })
     .use(prism)
     .process(markdown);
