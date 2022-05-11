814f4dccdcdd98483513feb9f01788a6a475b8ab
Maximilian Kaske <maximilian@kaske.org>
2022-05-11 22:35:00 +0200

    fix: missing metadata

--- apps/example/pages/index.tsx
+++ apps/example/pages/index.tsx
@@ -1,15 +1,37 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import { ConnectButton } from "widget";
 
+const locales = ["en", "de", "fr"] as const;
+
 const Home: NextPage = () => {
+  const [locale, setLocale] = useState<typeof locales[number]>("en");
   return (
-    <main className="flex min-h-screen min-w-screen justify-center items-center">
-      <ConnectButton
-        projectId={"cl2dnfmpg00788jik7de0lhz2"}
-        className="bg-indigo-500 rounded-full text-white px-3 py-2"
-      >
-        feedback
-      </ConnectButton>
+    <main className="flex flex-col min-h-screen min-w-screen justify-center items-center">
+      <div className="space-y-6 flex flex-col items-center">
+        <ConnectButton
+          projectId={"cl2dnfmpg00788jik7de0lhz2"}
+          lang={locale}
+          // show locale only if not default
+          metadata={locale !== "en" ? { locale } : undefined}
+          className="bg-indigo-500 rounded-full text-white px-3 py-2"
+        >
+          feedback
+        </ConnectButton>
+        <div className="space-x-3">
+          {locales.map((l) => (
+            <button
+              key={l}
+              onClick={() => setLocale(l)}
+              className={`rounded-full font-semibold uppercase px-2 py-1 ${
+                l === locale ? "bg-indigo-500 text-white" : "text-black"
+              }`}
+            >
+              {l}
+            </button>
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
