9b373d93112e247cec8cd5284834925b2118a867
Maximilian Kaske <maximilian@kaske.org>
2022-08-28 10:25:04 +0200

    fix: css

--- apps/example/pages/_app.tsx
+++ apps/example/pages/_app.tsx
@@ -2,9 +2,14 @@ import "../styles/globals.css";
 // import "../styles/widget.css"; // no need with tailwind v3.1+
 import "@fdbk/widget-react/dist/build.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" forcedTheme={"light"}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- apps/example/tailwind.config.js
+++ apps/example/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
